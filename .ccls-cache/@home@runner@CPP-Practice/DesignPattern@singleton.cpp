dc4ee386a5993226eadbc00250390bccc6207fec
Jay Agarwal <jaykissan@gmail.com>
2024-10-17 19:50:27 +0000

    changes till 1810
    changes till 1810

--- .ccls-cache/@home@runner@CPP-Practice/DesignPattern@singleton.cpp
+++ .ccls-cache/@home@runner@CPP-Practice/DesignPattern@singleton.cpp
@@ -1,62 +1,50 @@
 #if 1
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class singleton{
-    private:
-    static singleton *instance ;
+class Singleton {
+private:
     int data;
+    // Static pointer to the Singleton instance
+    static Singleton* instance;
+    // Private Constructor
+    Singleton() {}
 
-    private:
-    singleton(){};
-
-    public:
-    static singleton* getInstance(){
-        if(instance == NULL)
-        {
-          instance =  new singleton;
+public:
+    // Static method to get the Singleton instance
+    static Singleton* getInstance() {
+        if (instance == nullptr) {
+            instance = new Singleton();
         }
         return instance;
-    };
-    
-    int getdata(){
-      return data;
-    };
-    
-    void setdata(int d){
-     data = d;
-      retrun this 
-    };
-
-    void printdata(){
-      cout << "Data value : " << data << endl;
     }
-};//class singleton
 
-singleton * singleton::instance = NULL;
+    // Method to set values
+    void setData(int data) {
+        this->data = data;
+    }
 
-int main(){
-  singleton* s1 = singleton::getInstance();
-  singleton* s2 = singleton::getInstance();
-  singleton* s3 = singleton::getInstance();
-  singleton* s4 = singleton::getInstance();
-  singleton* s5 = singleton::getInstance();
-  singleton* s6 = singleton::getInstance();
-  
-  s1->setdata(10);
-  cout << s6->getdata();
-  cout << endl;
-  s2->setdata(20);
-  cout << s5->getdata();
-  cout << endl;
-  s3->setdata(30);
-  cout << s4->getdata();
-  cout << endl;
+    // Method to set values
+    int getData() {
+      return data;
+    }
 
-  singleton::getInstance()->setdata(10).printdata();
-  return 0;
+    // Method to print values
+    void print() const {
+        cout << "Values: " <<  data << endl;
+    }
+};
+
+// Initialize static members
+Singleton* Singleton::instance = nullptr;
+
+// Driver code
+int main() {
+    Singleton* s1 = Singleton::getInstance();
+    s1->setData(10);
+    s1->print();
+    return 0;
 }
-
 
 #endif
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
 	deps = [
+   pkgs.openssh
 		pkgs.clang
 		pkgs.ccls
 		pkgs.gdb
